5d123e856c5742e72761363e47ddc1286f3b2d33
Scott Fusilier <scottfusilier@gmail.com>
2016-03-19 15:31:47 -0400

    add provisions for calling anonymous functions in route handler info, update routes to closure function (see example App), clean up app class a bit

--- App.php
+++ App.php
@@ -13,16 +13,15 @@ use Lib\Handler\AppErrorHandlerInterface;
 
 class App
 {
-    /**
-     * @var instance The reference to Singleton instance of this class
-     */
+/**
+ * reference to Singleton instance of this class
+ */
     private static $instance;
 
-    /**
-     * Returns the Singleton instance of this class.
-     *
-     * @return Singleton of The App instance.
-     */
+/**
+ * Returns the Singleton instance of this class.
+ *
+ */
     public static function getInstance()
     {
         if (null === static::$instance) {
@@ -31,40 +30,32 @@ class App
         return static::$instance;
     }
 
-    /**
-     * Protected constructor to prevent creating a new instance of the
-     * App via the `new` operator from outside of this class.
-     */
+/**
+ * Protected constructor to prevent creating a new instance of the
+ */
     protected function __construct() {}
 
-    /**
-     * Private clone method to prevent cloning of the instance of the
-     * App instance.
-     *
-     * @return void
-     */
+/**
+ * Private clone method to prevent cloning of the instance of the
+ * App instance.
+ *
+ */
     private function __clone() {}
 
-    /**
-     * Private unserialize method to prevent unserializing of the App
-     * instance.
-     *
-     * @return void
-     */
+/**
+ * Private unserialize method to prevent unserializing of the App
+ * instance.
+ *
+ */
     private function __wakeup() {}
 
-    /**
-     * Run the Application
-     *
-     * @param array
-     * @return vcid
-     */
-    public function run(array $routes)
+/**
+ * Run the Application
+ *
+ */
+    public function run($routes)
     {
-        $routeCollector = new RouteCollector(new Std, new GroupCountBased);
-        foreach ($routes as $route) {
-            $routeCollector->addRoute($route[0], $route[1], $route[2]);
-        }
+        $routeCollector = $routes(new RouteCollector(new Std, new GroupCountBased));
 
         $request = Request::createFromGlobals();
         AppContainer::register($request);
@@ -75,62 +66,116 @@ class App
         $dispatcher = new DispatcherGroupCountBased($routeCollector->getData());
         $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
-        return $this->dispatchController($routeInfo);
+        return $this->dispatchRoute($routeInfo);
     }
 
-    /**
-     * Dispatch Controller based on route match
-     *
-     * @param array
-     * @return void
-     */
-    private function dispatchController(array $routeInfo)
+/**
+ * Dispatch route match
+ *
+ */
+    private function dispatchRoute(array $routeInfo)
+    {
+        switch ($routeInfo[0]) {
+            case Dispatcher::METHOD_NOT_ALLOWED :
+                return self::handleMethodNotAllowed($routeInfo[1]);
+
+            case Dispatcher::NOT_FOUND :
+                return self::handleNotFound($routeInfo);
+
+            case Dispatcher::FOUND :
+                return self::handleFound($routeInfo);
+        }
+
+        $response = new Response();
+        $response->setStatusCode(500);
+
+        if (AppContainer::isRegistered('AppErrorHandler') && AppContainer::getInstance('AppErrorHandler') instanceOf AppErrorHandlerInterface) {
+            ob_start();
+            AppContainer::getInstance('AppErrorHandler')->handleAppError();
+            $response->setContent(ob_get_clean());
+        } else {
+            $response->setContent('');
+        }
+
+        return $response->send();
+    }
+
+/**
+ * HTTP Method Not Allowed
+ */
+    private function handleMethodNotAllowed(array $routeInfo)
     {
         $response = new Response();
         AppContainer::register($response);
 
-        if ($routeInfo[0] == Dispatcher::METHOD_NOT_ALLOWED) {
-            $allowedMethods = $routeInfo[1];
-            $response->setStatusCode(405);
+        $allowedMethods = $routeInfo;
+
+        $response->setStatusCode(405);
+        $response->setContent('');
+
+        return $response->send();
+    }
+
+/**
+ * Route Match Not Found
+ */
+    private function handleNotFound(array $routeInfo)
+    {
+        $response = new Response();
+        AppContainer::register($response);
+
+        $response->setStatusCode(404);
+
+        if (AppContainer::isRegistered('AppErrorHandler') && AppContainer::getInstance('AppErrorHandler') instanceOf AppErrorHandlerInterface) {
+            ob_start();
+            AppContainer::getInstance('AppErrorHandler')->handleNotFound();
+            $response->setContent(ob_get_clean());
+        } else {
             $response->setContent('');
-            $response->send();
-            exit();
         }
 
-        if ($routeInfo[0] == Dispatcher::NOT_FOUND) {
-            $response->setStatusCode(404);
-            if (AppContainer::isRegistered('AppErrorHandler') && AppContainer::getInstance('AppErrorHandler') instanceOf AppErrorHandlerInterface) {
-                ob_start();
-                AppContainer::getInstance('AppErrorHandler')->handleNotFound();
-                $response->setContent(ob_get_clean());
-            } else {
-                $response->setContent('');
-            }
-            $response->send();
-            exit();
-        }
+        return $response->send();
+    }
 
-        if ($routeInfo[0] == Dispatcher::FOUND) {
-            // collect all vars
-            $vars = $routeInfo[2];
-            if (!empty($_POST)) {
+/**
+ * Handle Matched Route
+ */
+    private static function handleFound($routeInfo)
+    {
+        $response = new Response();
+        AppContainer::register($response);
+
+        // collect all vars
+        $vars = $routeInfo[2];
+        if (!empty($_POST)) {
             $vars += $_POST;
-            }
-            if (!empty($_GET)) {
-            $vars += $_GET;
-            }
-            unset($_POST);
-            unset($_GET);
-            // get handler for route
-            $handler = $routeInfo[1];
-            $info = explode('::', $handler);
-            $namespace = $info[0];
-            $action = $info[1];
         }
+
+        if (!empty($_GET)) {
+            $vars += $_GET;
+        }
+
+        // we handle vars here and pass them into the function call for devs
+        unset($_POST);
+        unset($_GET);
+
+        // get handler for route
+        $handler = $routeInfo[1];
+
+        // route override as closure, call it with variables
+        if (is_object($handler) && (new \ReflectionFunction($handler))->isClosure()) {
+            $response->setContent($handler($vars));
+            return $response->send();
+        }
+
+        // handle regular controller based route
+        $info = explode('::', $handler);
+        $namespace = $info[0];
+        $action = $info[1];
 
         $controller = new $namespace();
         ob_start();
-
+        // call with variables
         $controller->{$action}($vars);
 
         $response->setContent(ob_get_clean());
